64018633662705f98b002ecd40c3666655b013e0
Tim Braun <tim@tim-braun.com>
2022-04-02 15:28:13 -0500

    Add language support and use search path for dictionary.

--- passphrase.py
+++ passphrase.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-#* 
+#*
 #* passPhraseGenerator - Generate pronounceable passwords
 #  This program uses a random series of short dictionary words
 #
@@ -11,35 +11,55 @@
 
 from random import choice
 import argparse
+import os.path
 
-DICT = '/usr/share/hunspell/en_CA.dic'
+DICTS = [ '/usr/share/hunspell', '/usr/share/myspell' ]
 ALTCHARS = '-+_%'
 
 parser = argparse.ArgumentParser(description='Generate some random passwords')
-parser.add_argument('--numbers', help='l33t numbers', action='store_true', default=True)
-parser.add_argument('-n', '--no-numbers', help='no l33t numbers', action='store_false', dest='numbers')
-parser.add_argument('--caps', help='Capitalization', action='store_true', default=True)
-parser.add_argument('-c', '--no-caps', help='no capitalization', action='store_false', dest='caps')
-parser.add_argument('--altchars', help='alternate characters', action='store_true', default=True)
-parser.add_argument('-a', '--no-altchars', help='no alternate characters', action='store_false', dest='altchars')
-parser.add_argument('--count', help='how many passwords do you want', default=5, type=int)
-parser.add_argument('--length', help='minimum password length', default=12, 
+parser.add_argument('--numbers', help='l33t numbers',
+	action='store_true', default=True)
+parser.add_argument('-n', '--no-numbers', help='no l33t numbers',
+	action='store_false', dest='numbers')
+parser.add_argument('--caps', help='Capitalization',
+	action='store_true', default=True)
+parser.add_argument('-c', '--no-caps', help='no capitalization',
+	action='store_false', dest='caps')
+parser.add_argument('--altchars', help='alternate characters',
+	action='store_true', default=True)
+parser.add_argument('-a', '--no-altchars', help='no alternate characters',
+	action='store_false', dest='altchars')
+parser.add_argument('--count', help='how many passwords do you want',
+	default=5, type=int)
+parser.add_argument('--length', help='minimum password length', default=12,
 	dest='phrase_min', type=int)
-parser.add_argument('--max-length', help='maximum password length', default=18, 
+parser.add_argument('--max-length', help='maximum password length', default=18,
 	dest='phrase_max', type=int)
 parser.add_argument('--word-max', help='maximum word length', default=6, type=int)
 parser.add_argument('--word-min', help='minimum word length', default=2, type=int)
+parser.add_argument('-l', '--lang', help='language the password is base on',
+	choices=[ 'en_CA', 'fr_CA', 'sw_TZ' ], default='en_CA')
 args = parser.parse_args()
 
+def get_dict(lang):
+	for dic_path in DICTS:
+		dict_file = os.path.join( dic_path, lang + '.dic' )
+		if os.path.exists( dict_file ):
+			return dict_file
+	print('Dictionary for', lang, 'not found')
+	exit(2)
+
 if (args.word_min > args.word_max):
-	print('Impossible options, word_min', args.word_min, '> word_max', args.word_max)
+	print('Impossible options, word_min', args.word_min,
+		'> word_max', args.word_max)
 	exit(1)
 
 if (args.phrase_min > args.phrase_max):
-	print('Impossible options, phrase_min', args.phrase_min, '> phrase_max', args.phrase_max)
+	print('Impossible options, phrase_min', args.phrase_min,
+		'> phrase_max', args.phrase_max)
 	exit(1)
 
-with open(DICT, 'r') as file:
+with open(get_dict(args.lang), 'r') as file:
 	dict = file.readlines(-1)
 
 def clean():
@@ -60,28 +80,30 @@ def phrase(altchars, caps):
 		new_word = random_word(altchars and (not "'" in p))
 		p =  p + (new_word.capitalize() if caps else new_word)
 		if ((((args.phrase_min - len(p)) < args.word_min) and
-			len(p) < args.phrase_min) or 
+			len(p) < args.phrase_min) or
 			(len(p) > args.phrase_max)):
 			# start over
-			# print('reset: min', args.phrase_min, 'len(p)', len(p), 'word_min', args.word_min)
+			# print('reset: min', args.phrase_min,
+			#		'len(p)', len(p), 'word_min', args.word_min)
 			# print('  p', p, 'phrase_max', args.phrase_max)
 			p = random_word()
 	return p.replace("'", choice( ALTCHARS )) if altchars else p
 
-# get a digit somehow - t to 7, b to 8, a to 4, s to 5,e to 3 
+# get a digit somehow - t to 7, b to 8, a to 4, s to 5,e to 3
 def l33tphrase(altchars, caps):
 	w = phrase(altchars, caps)
 	if 't' in w:
-		return w.replace('t', '7', 1) 
+		return w.replace('t', '7', 1)
 	if 'b' in w:
-		return w.replace('b', '8', 1) 
+		return w.replace('b', '8', 1)
 	if 'a' in w:
-		return w.replace('a', '4', 1) 
+		return w.replace('a', '4', 1)
 	if 's' in w:
-		return w.replace('s', '5', 1) 
+		return w.replace('s', '5', 1)
 	return w.replace('e', '3', 1)
 
 
 
 for i in range(args.count):
-	print( l33tphrase(args.altchars, args.caps) if args.numbers else phrase(args.altchars, args.caps) )
+	print( l33tphrase(args.altchars, args.caps)
+		if args.numbers else phrase(args.altchars, args.caps) )
